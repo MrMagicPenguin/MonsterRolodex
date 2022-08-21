09f47a6e280098e977952d95f3ca985eb656b4d9
voodoo <nprice@pricemail.ca>
2022-08-21 14:25:44 -0400

    Add query system.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super();
+        this.fruits = ['apple', 'banana', 'mango', 'grapes']
+        this.state = {
+            monsters: []
+        }
+        console.log("constructor")
+    }
+
+    // * Happens first time the Component renders
+    // * stick API Calls in this.
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((users) => this.setState(() => {
+                    return {monsters: users}
+                },
+                () => console.log(this.state)
+                )
+            )
+        console.log("componentDidMount")
+    }
+
+    // Takes in onChange event and State
+    // Returns shallow copy of state where query matches entry in state.
+    // updates state
+    filterMonsters(arr, query) {
+        return arr.filter((el) => el.name.toLowerCase().includes(query.toLowerCase()))
+
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <div className="App">
+                <input className='search-box'
+                       type='search'
+                       placeholder='Search Monsters'
+                       onChange={(e) => {
+                            this.setState(() => {
+                                        return {monsters: this.filterMonsters(this.state.monsters, e.target.value)}
+                                        }
+                                )
+                            }
+                       }
+                />
+                {
+                    this.state.monsters.map((monster) => {
+                            return <h1 key={monster.name}>{monster.name}</h1>
+                        }
+                    )
+                }
+
+            </div>
+        )
+
+    }
+
 }
+
 
 export default App;
